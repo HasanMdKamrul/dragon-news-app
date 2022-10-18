76d98b8a76672157b5b8bc37645733487189a152
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-18 06:39:49 +0200

    Basic setup with the layout and folder structure added

--- src/Routes/Router/Router.js
+++ src/Routes/Router/Router.js
@@ -1,10 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layouts/Main";
+import Category from "../../page/Category/Category/Category";
+import Home from "../../page/Home/Home/Home";
+import News from "../../page/News/News/News";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    children: [{}],
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/news/:newsId",
+        element: <News />,
+      },
+      {
+        path: "/category/:categoryId",
+        element: <Category />,
+      },
+    ],
   },
 ]);
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import Footer from "../page/Shared/Footer/Footer";
+import Header from "../page/Shared/Header/Header/Header";
+import LeftSideNav from "../page/Shared/LeftSideNav/LeftSideNav";
+import RightSideNav from "../page/Shared/RightSideNav/RightSideNav";
 
 const Main = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <Outlet />
+      <Header />
+      <Container>
+        <Row>
+          <Col lg="2">
+            <LeftSideNav />
+          </Col>
+          <Col lg="7">
+            <Outlet />
+          </Col>
+          <Col lg="3">
+            <RightSideNav />
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
     </div>
   );
 };
